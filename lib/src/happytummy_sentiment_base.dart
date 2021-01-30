ac38ec3c1ab47aec455d765e53804afa5668717e
suyogguragain <43370904+suyogguragain@users.noreply.github.com>
2021-01-30 10:51:41 +0545

    Update happytummy_sentiment_base.dart

--- lib/src/happytummy_sentiment_base.dart
+++ lib/src/happytummy_sentiment_base.dart
@@ -67,6 +67,70 @@ class Sentiment {
           }
         });
       }
+      var result = {
+        'score': score,
+        //'comparative': wordlist.isNotEmpty ? score / wordlist.length : 0,
+        //'words': wordlist,
+        //'good words': goodwords,
+        //'badword': badwords
+      };
+      return result;
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+  
+  int analysis1(String text,
+      {Map customLang, bool emoji = false, String languageCode}) {
+    try {
+      if (text.isEmpty) throw ('err');
+      languageCode ??= 'en';
+      var sentiments = {};
+      if (emoji) sentiments.addAll(emojis);
+      if (customLang == null) {
+        switch (languageCode) {
+
+          /// english
+          case 'en':
+            sentiments.addAll(en);
+            break;
+
+          default:
+            throw ('err');
+        }
+      } else {
+        sentiments.addAll(customLang);
+      }
+      var score = 0;
+      var goodwords = [], badwords = [];
+      var wordlist = emoji
+          ? text
+              .toLowerCase()
+              .replaceAll('\n', ' ')
+              .replaceAll('s\s+', ' ')
+              .replaceAll(RegExp(r'[.,\/#!?$%\^&\*;:{}=_`\"~()]'), '')
+              .trim()
+              .split(' ')
+          : text
+              .toLowerCase()
+              .replaceAll('\n', ' ')
+              .replaceAll('s\s+', ' ')
+              .replaceAll(RegExp(r'[.,\/#!?$%\^&\*;:{}=_`\"~()]'), '')
+              .removemoji
+              .trim()
+              .split(' ');
+      for (var i = 0; i < wordlist.length; i++) {
+        sentiments.forEach((key, value) {
+          if (key == wordlist[i]) {
+            score += value;
+            if (value < 0) {
+              badwords.add([key, value]);
+            } else {
+              goodwords.add([key, value]);
+            }
+          }
+        });
+      }
 //       var result = {
 //         'score': score,
 //         //'comparative': wordlist.isNotEmpty ? score / wordlist.length : 0,
